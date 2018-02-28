946ae0d203a51682cfeb80f769d38f4c12f21945
AfrazAhmad <afraz.ahmad@cognitiveaxis.com>
2018-02-28 15:16:08 +0500

    changing directory route in autload of composer.json

--- src/ClearAllCachedDataProvider.php
+++ src/ClearAllCachedDataProvider.php
@@ -2,7 +2,6 @@
 
 namespace AfrazAhmad\ClearAllCachedData;
 
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use AfrazAhmad\ClearAllCachedData\ClearAllCachedData;
 
@@ -15,19 +14,7 @@ class ClearAllCachedDataProvider extends ServiceProvider
      */
     public function boot()
     {
-        // load routes
-            //$this->loadRoutesFrom(__DIR__.'/routes.php');
-        //include __DIR__.'/routes.php';
-        // register our controller
-            // $this->app->make('Devdojo\Calculator\CalculatorController');
-        // load view
-            // $this->loadViewsFrom(__DIR__.'/views', 'calculator');
-            // $this->publishes([__DIR__.'/path/to/views' => resource_path('views/vendor/courier'),
-        // load migrations
-            // $this->loadMigrationsFrom(__DIR__.'/path/to/migrations');
-        // load translations
-            // $this->loadTranslationsFrom(__DIR__.'/path/to/translations', 'courier');
-            // $this->publishes([__DIR__.'/path/to/translations' => resource_path('lang/vendor/courier'),
+        
         if ($this->app->runningInConsole()) {
             $this->commands([
                 ClearAllCachedData::class,
